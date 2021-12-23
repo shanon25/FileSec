5343b7a5a5a1890660e580c046fe989937d83216
shanon25 <59134659+shanon25@users.noreply.github.com>
2021-12-24 01:49:15 +0530

    added a menu bar

--- main.py
+++ main.py
@@ -12,15 +12,13 @@ app = root.minsize(600, 900)
 label1 = Label(root, text="Upload a PDF or Word document to scan for Malware", font="Raleway")
 label1.place(x=118, y=270)
 
-
 # uploading function
 def Uploading():
     text.set("Uploading...")
-    file = askopenfile(parent=root, mode='rb', title="Choose file", filetypes=[("PDF file", "*.pdf")])
+    file = askopenfile(parent=root, mode='rb', title="Choose file", filetypes=[("Word Document","*.docx"), ("PDF file", "*.pdf")])
 
     if file:
         text.set("Click to Scan")
-        text.set("Scanning...")
 
     else:
         text.set("No files were selected!")
@@ -35,5 +33,30 @@ button1 = Button(root, textvariable=text, text="Upload", command=lambda: Uploadi
                  height=2, bg="light blue")
 button1.place(x=260, y=310)
 text.set("Upload")
+
+#Menue
+menu = Menu(root)
+root.config(menu=menu)
+
+#menu items
+scan_menu = Menu(menu)
+menu.add_cascade(label="Scans", menu=scan_menu)
+scan_menu.add_command(label="Basic Scan")
+scan_menu.add_command(label="Advanced Scan")
+scan_menu.add_separator()
+scan_menu.add_command(label="Exit", command=root.quit)
+
+#report generator menu
+report_menu = Menu(menu)
+menu.add_cascade(label="Reports", menu=report_menu)
+report_menu.add_command(label="PDF Reports")
+report_menu.add_command(label="Word Document")
+report_menu.add_command(label="CSV")
+
+#help and about menu
+help_menu = Menu(menu)
+menu.add_cascade(label="Info", menu=help_menu)
+help_menu.add_command(label="Help")
+help_menu.add_command(label="About FileSec")
 
 root.mainloop()
