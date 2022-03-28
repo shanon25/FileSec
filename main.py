4c543207fb2fecc38fed2aedf4568e0b1a816994
shanon25 <59134659+shanon25@users.noreply.github.com>
2022-03-29 00:31:39 +0530

    Created a requirement.txt file

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import tkinter
-
 from tkinter import *
-import requests
 from tkinter import filedialog
 from tkinter import messagebox
 import string
+import threading
 import time
+from tkinter.filedialog import askopenfile
 import backend
 from backend import *
 from ctypes import windll
@@ -20,31 +19,33 @@ app = root.minsize(600, 750)
 
 # label
 label1 = Label(root, text="Upload a PDF or Word document to scan for Malware", font="Raleway")
-label1.place(x=118, y=150)
+label1.place(x=100, y=150)
 
 
 # uploading function
 def Uploading():
     text.set("Uploading...")
     file = filedialog.askopenfilename(initialdir="/", title="Choose file",
-                       filetypes=[("Word Document", "*.docx"), ("PDF file", "*.pdf")])
+                                      filetypes=[("Word Document", "*.docx"), ("PDF file", "*.pdf")])
     # filename label
     file_lbl = Label(root, fg="black")
     file_lbl.place(x=90, y=280)
-    file_lbl.configure(text="File: "+file)
-
+    file_lbl.configure(text="File: " + file)
 
     if file:
-        #button 2 for send the selected file to Virus total
+        # button 2 for send the selected file to Virus total
         text.set("Click to Scan")
-        button2 = Button(root, textvariable=text, text="Upload", command=lambda : showresult(file), font="Raleway", width=10, height=2,
+        button2 = Button(root, textvariable=text, text="Upload", command=lambda: showresult(file), font="Raleway",
+                         width=10, height=2,
                          bg="light blue")
         button2.place(x=260, y=200)
 
     else:
         text.set("Upload")
         messagebox.showerror("Error", "Please select a file!!!")
-        file = askopenfile(parent=root, mode='rb', title="Choose file", filetypes=[("Word Document", "*.docx"), ("PDF file", "*.pdf")])
+        file = askopenfile(parent=root, mode='rb', title="Choose file",
+                           filetypes=[("Word Document", "*.docx"), ("PDF file", "*.pdf")])
+
 
 def showresult(file):
     result = backend.execute(file)
@@ -53,15 +54,20 @@ def showresult(file):
 
 # button 1 for uploading a file
 text = StringVar()
-button1 = Button(root, textvariable=text, text="Upload", command=Uploading, font="Raleway", width=10, height=2, bg="light blue")
+button1 = Button(root, textvariable=text, text="Upload", command=Uploading, font="Raleway", width=10, height=2,
+                 bg="light blue")
 button1.place(x=260, y=200)
 text.set("Upload")
 
+
 def show_results_in_ui(data):
-    sc_verbose_lbl = Label(root, text=f"Message: {data.get('verbose_msg')}", font="Raleway", bg="light green", fg="black")
+    sc_verbose_lbl = Label(root, text=f"Message: {data.get('verbose_msg')}", font="Raleway", bg="light green",
+                           fg="black")
     sc_total_lbl = Label(root, text=f"Total: {data.get('total')}", font="Raleway", bg="light green", fg="black")
-    sc_pos_lbl = Label(root, text=f"Positives: {data.get('positives')}", font="Raleway", bg="light green", fg="black")
-    sc_date_lbl = Label(root, text=f"Scan date: {data.get('scan_date')}", font="Raleway", bg="light green", fg="black")
+    sc_pos_lbl = Label(root, text=f"Positives: {data.get('positives')}", font="Raleway", bg="light green",
+                       fg="black")
+    sc_date_lbl = Label(root, text=f"Scan date: {data.get('scan_date')}", font="Raleway", bg="light green",
+                        fg="black")
     sc_verbose_lbl.place(x=90, y=350)
     sc_total_lbl.place(x=90, y=400)
     sc_pos_lbl.place(x=90, y=450)
@@ -71,8 +77,10 @@ def show_results_in_ui(data):
         sc_mal_lbl = Label(root, text="MALWARE DETECTED!!!", bg="brown", fg="white")
         sc_mal_lbl.place(x=140, y=550)
     else:
-        sc_mal_lbl = Label(root, text="NO MALWARE IS DETECTED, FILE IS SAFE", bg="light green", fg="black", font="Raleway")
+        sc_mal_lbl = Label(root, text="NO MALWARE IS DETECTED, FILE IS SAFE", bg="light green", fg="black",
+                           font="Raleway")
         sc_mal_lbl.place(x=140, y=550)
+
 
 # Advanced scanning window
 def adv_scan():
@@ -80,6 +88,7 @@ def adv_scan():
     adv.maxsize(600, 900)
     adv.minsize(600, 900)
     adv.title("Advanced Scan")
+
 
 # Menu
 menu = Menu(root)
@@ -98,17 +107,22 @@ def quit_form():
 # USB scanning function
 text2 = StringVar()
 text3 = StringVar()
+
+
 def scan():
-    pop = Toplevel()
-    pop.title("USB scanner")
-    pop.maxsize(700, 500)
-    pop.minsize(700, 500)
-    file_txt = Listbox(pop, width=90, height=20)
+    pop_global = Toplevel()
+    pop_global.title("USB scanner")
+    pop_global.maxsize(700, 500)
+    pop_global.minsize(700, 500)
+    file_txt = Listbox(pop_global, width=90, height=20)
     file_txt.place(x=80, y=160)
-    btn_scn = Button(pop, textvariable=text2, text="List the files", command=lambda : Filetype(pop), font="Raleway", width=10, height=2, bg="brown", fg="white")
+    btn_scn = Button(pop_global, textvariable=text2, text="List the files", command=lambda: Filetype(pop_global),
+                     font="Raleway",
+                     width=10, height=2, bg="brown", fg="white")
     btn_scn.place(x=300, y=50)
     text2.set("Scan the files")
-    btn1_scn = Button(pop, textvariable=text3, text="Scan", font="Raleway", width=10, height=2, bg="brown", fg="white")
+    btn1_scn = Button(pop_global, textvariable=text3, text="Scan", font="Raleway", width=10, height=2, bg="brown",
+                      fg="white")
     text3.set("Scan")
 
 
@@ -122,37 +136,63 @@ def status():
         record_deviceBit >>= 1
     return devices
 
+
+# def detect_device():
+#     original = set(status())
+#     print('Detecting...')
+#     time.sleep(3)
+#     add_device = set(status()) - original
+#     subt_device = original - set(status())
+#
+#     if len(add_device):
+#         print("There were %d" % (len(add_device)))
+#         for drive in add_device:
+#             print("The drives added: %s." % drive)
+#
+#     elif len(subt_device):
+#         print("There were %d" % (len(subt_device)))
+#         for drive in subt_device:
+#             print("The drives remove: %s." % drive)
+
+#dirname = "D:\\Files\\"
+
+
 def detect_device():
-    original = set(status())
-    print('Detecting...')
-    time.sleep(3)
-    add_device = set(status()) - original
-    subt_device = original - set(status())
+    global dirname
+    while True:
+        original = set(status())
+        print('Detecting...')
+        time.sleep(1)
+        add_device = set(status()) - original
+        subt_device = original - set(status())
 
-    if len(add_device):
-        print("There were %d" % (len(add_device)))
-        for drive in add_device:
-            print("The drives added: %s." % drive)
+        if len(add_device):
+            print("There were %d" % (len(add_device)))
+            for drive in add_device:
+                print("The drives added: %s." % drive)
+            dirname = f'{drive}:\\'
+            usb()
 
-    elif len(subt_device):
-        print("There were %d" % (len(subt_device)))
-        for drive in subt_device:
-            print("The drives remove: %s." % drive)
+        elif len(subt_device):
+            print("There were %d" % (len(subt_device)))
+            for drive in subt_device:
+                print("The drives remove: %s." % drive)
 
-#Listing down PDF and word files in an external drive
+
+# Listing down PDF and word files in an external drive
 def Filetype(pop):
-    device = detect_device()
+    print(dirname)
+    # device = detect_device()
     file_txt = Listbox(pop, width=90, height=20)
     file_txt.place(x=80, y=160)
+    file_txt.delete(0, END)
     filenames = {}
-    dirname = os.chdir(device)
     ext = ('.pdf', '.docx')
     for root, dirs, files in os.walk(dirname):
         for index, file in enumerate(files):
             if file.endswith(ext):
                 filenames[file] = f"{root}{file}"
                 file_txt.insert(index, file)
-
     counter = 0
     for fname, fpath in filenames.items():
         result = backend.execute(fpath)
@@ -169,11 +209,12 @@ def Filetype(pop):
         file_txt.insert(counter, label_text)
         counter += 1
 
+
 # USB plugin message box
 def usb():
     device = status()
-    txt = StringVar()
-    res = messagebox.askyesno("USB Detected!!", "USB is detected please click YES to scan all the PDFs and Word Documents in the device")
+    res = messagebox.askyesno("USB Detected!!",
+                              "USB is detected please click YES to scan all the PDFs and Word Documents in the device")
     if res == 1:
         scan()
     else:
@@ -207,6 +248,21 @@ menu.add_cascade(label="Info", menu=help_menu)
 help_menu.add_command(label="Help")
 help_menu.add_command(label="About FileSec")
 
-usb()
+
+class RunFunctionInBackground(threading.Thread):
+
+    def __init__(self, function, *args, **kwargs):
+        threading.Thread.__init__(self)
+        self.runnable = function
+        self.daemon = True
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        self.runnable(*self.args, **self.kwargs)
+
+
+thread = RunFunctionInBackground(detect_device)
+thread.start()
 
 root.mainloop()
